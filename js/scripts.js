06f9593fff06622d02a73934ee80860b32f1b1d0
brlopez192 <brlopez192@gmail.com>
2024-11-19 17:43:20 -0500

    Updated PokemonList
    Added 3 Pokemon objects the the PokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [];
+let pokemonList = [
+{name: 'Bulbasaur', Height: 7, type: ['Grass', 'Monster']}
+{name: 'Charizard', Height: 17, type: ['Dragon', 'Monster']}
+{name: 'Squirtle', Height: 5, type: ['Water 1', 'Monster']}
+];
