47797e0d12e2e3fefc25f93a4f549efd4951b817
brlopez192 <brlopez192@gmail.com>
2024-11-19 17:44:52 -0500

    fixed errors in pokemonList
    added commas to fix PokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-{name: 'Bulbasaur', Height: 7, type: ['Grass', 'Monster']}
-{name: 'Charizard', Height: 17, type: ['Dragon', 'Monster']}
+{name: 'Bulbasaur', Height: 7, type: ['Grass', 'Monster']},
+{name: 'Charizard', Height: 17, type: ['Dragon', 'Monster']},
 {name: 'Squirtle', Height: 5, type: ['Water 1', 'Monster']}
 ];
