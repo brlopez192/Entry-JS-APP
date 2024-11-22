ed719089259b92d3b106dc9393f7746ae254a6db
brlopez192 <brlopez192@gmail.com>
2024-11-21 19:21:22 -0500

    creating for loops
    Creating two for loops to list out pokemon and determine which is the biggest

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 let pokemonList = [
-{name: 'Bulbasaur', Height: 7, type: ['Grass', 'Monster']},
-{name: 'Charizard', Height: 17, type: ['Dragon', 'Monster']},
-{name: 'Squirtle', Height: 5, type: ['Water 1', 'Monster']}
+{name: 'Bulbasaur', 
+    Height: 7, 
+    type: ['Grass', 'Monster']},
+{name: 'Charizard', 
+    Height: 17, 
+    type: ['Dragon', 'Monster']},
+{name: 'Squirtle', 
+    Height: 5, 
+    type: ['Water 1', 'Monster']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name}: ${pokemonList[i].Height}, `)
+}
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].Height > 10) {
+        document.write(`${pokemonList[i].name} is the Biggest Pokemon`)
+    }
+}
