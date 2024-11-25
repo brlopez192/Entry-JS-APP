04f0c460e130a87b3ef7c320e6f085dc2b390f70
brlopez192 <brlopez192@gmail.com>
2024-11-24 20:54:52 -0500

    Wrapped PokemonList into an IIFE
    Wrapped Pokemon list into an IIFE called pokemonRepository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
 // Declaration of pokemon array with each object
-let pokemonList = [
-{name: 'Bulbasaur', 
-    Height: 7, 
-    type: ['Grass', 'Monster']},
-{name: 'Charizard', 
-    Height: 17, 
-    type: ['Dragon', 'Monster']},
-{name: 'Squirtle', 
-    Height: 5, 
-    type: ['Water 1', 'Monster']}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', 
+            Height: 7, 
+            type: ['Grass', 'Monster']},
+        {name: 'Charizard', 
+            Height: 17, 
+            type: ['Dragon', 'Monster']},
+        {name: 'Squirtle', 
+            Height: 5, 
+            type: ['Water 1', 'Monster']}
+        ];
+        return {
+            add: function() {
+                pokemonList.push(pokemon);
+            },
+            getAll: function() {
+                return pokemonList;
+            }
+        };
+})();
 
 // For loop to print out the details of each pokemon
 // for (let i = 0; i < pokemonList.length; i++) {
 //     document.write("<p>" + `${pokemonList[i].name}: ${pokemonList[i].Height}` + "</p>")
 // }
 
-pokemonList.forEach(function(name) {
-    document.write(`${name}`);
-})
+// ForEach method to print out details of each pokemon
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write("<p>" + `${pokemon.name}: ${pokemon.Height}`);
+});
 
 // For loop to print out which pokemon is the biggest
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].Height > 10) {
-        document.write("<p>" + `${pokemonList[i].name} is the Biggest Pokemon` + "</p>")
+for (let i = 0; i < pokemonRepository.getAll().length; i++) {
+    if (pokemonRepository.getAll()[i].Height > 10) {
+        document.write("<p>" + `${pokemonRepository.getAll()[i].name} is the Biggest Pokemon` + "</p>")
     }
 }
