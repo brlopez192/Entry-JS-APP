68ac1fa6d2b5bc42878515a4017bc9550d74e28f
brlopez192 <brlopez192@gmail.com>
2024-12-05 00:02:56 -0500

    adding Bootstrap Modal
    Adding Bootstrap modal and eplacing existing code to run with new bootstrap cood

--- js/scripts.js
+++ js/scripts.js
@@ -23,61 +23,37 @@ let pokemonRepository = (function() {
             let pokemonList = document.querySelector('.pokemon-list');
             let listPokemon = document.createElement("li");
             let button = document.createElement("button");
+            listPokemon.classList.add("list-group-item");
             button.innerText = pokemon.name;
-            button.classList.add("button-class");
+            button.classList.add("btn", "btn-primary");
             button.addEventListener('click', function () {
                 showDetails(pokemon);
             });
             listPokemon.appendChild(button);
             pokemonList.appendChild(listPokemon);
+            button.addEventListener("click", function(event){
+                pokemonRepository.showDetails(pokemon)
+            })
         }
 
         function showModal(title, text, img) {
-            let modalContainer = document.querySelector('#modal-container');
-            modalContainer.innerText = '';
-            let modal = document.createElement('div');
-            modal.classList.add('modal');
-            let closeButtonElement = document.createElement('button');
-
-            closeButtonElement.classList.add('modal-close');
-            closeButtonElement.innerText = 'Close';
-            closeButtonElement.addEventListener('click', hideModal);
+            let modalTitle = document.querySelector("#pokemonModalLabel");
+            let modalBody = document.querySelector(".modal-body");
+            let pokemonHeight = document.querySelector("#pokemonHeight");
+            let pokemonImage = document.querySelector("#pokemonImage");
       
-            let pokemonName = document.createElement('h1');
-            pokemonName.innerText = title;
-            let pokemonHeight = document.createElement('p');
+            modalTitle.innerText = title;
             pokemonHeight.innerText = text;
-            let pokemonImage = document.createElement('img');
-
             pokemonImage.setAttribute('src', img);
-            pokemonImage.setAttribute('width', '100%');
-            pokemonImage.setAttribute('height', '100%');
-      
-            modal.appendChild(closeButtonElement);
-            modal.appendChild(pokemonName);
-            modal.appendChild(pokemonHeight);
-            modal.appendChild(pokemonImage);
-            modalContainer.appendChild(modal);
-      
-            modalContainer.classList.add('is-visible');
-      
-            modalContainer.addEventListener('click', (e) => {
-              let target = e.target;
-              if (target === modalContainer) {
-                hideModal();
-              }
-            });
           }
-          function hideModal() {
-            let modalContainer = document.querySelector('#modal-container');
-            modalContainer.classList.remove('is-visible');
-          }
+
 
           function showDetails(pokemon) {
             loadDetails(pokemon).then(function () {
               showModal( pokemon.name,
                 "Height: " + pokemon.height,
                 pokemon.imageURL);
+                $("#pokemonModal").modal('show');
             });
           }
 
@@ -118,11 +94,6 @@ let pokemonRepository = (function() {
         };
 })();
 
-// For loop to print out the details of each pokemon
-// for (let i = 0; i < pokemonList.length; i++) {
-//     document.write("<p>" + `${pokemonList[i].name}: ${pokemonList[i].Height}` + "</p>")
-// }
-
 // ForEach method to print out details of each pokemon
 pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function(pokemon) {
@@ -130,9 +101,3 @@ pokemonRepository.loadList().then(function() {
     });
 });
 
-// For loop to print out which pokemon is the biggest
-// for (let i = 0; i < pokemonRepository.getAll().length; i++) {
-//     if (pokemonRepository.getAll()[i].Height > 10) {
-//         document.write("<p>" + `${pokemonRepository.getAll()[i].name} is the Biggest Pokemon` + "</p>")
-//     }
-// }
